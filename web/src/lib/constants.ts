86634aa51059eafac653a7893db3b03d8bb793e7
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-04 22:36:50 +0700

    fix(web): PUBLIC_LOGIN_PAGE_MESSAGE

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const loginPageMessage: string = import.meta.env.VITE_LOGIN_PAGE_MESSAGE;
+// export const loginPageMessage: string = import.meta.env.VITE_LOGIN_PAGE_MESSAGE;
+import { env } from '$env/dynamic/public';
+export const loginPageMessage: string = env.PUBLIC_LOGIN_PAGE_MESSAGE;
