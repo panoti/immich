935c3ae35189a132686efae71e1fba37e312b56f
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-05 16:00:38 +0700

    refactor(web): rollback

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,3 +1,1 @@
-// export const loginPageMessage: string = import.meta.env.VITE_LOGIN_PAGE_MESSAGE;
-import { env } from '$env/dynamic/public';
-export const loginPageMessage: string = env.PUBLIC_LOGIN_PAGE_MESSAGE;
+export const loginPageMessage: string = import.meta.env.VITE_LOGIN_PAGE_MESSAGE;
