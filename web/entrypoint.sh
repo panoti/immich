2cd3947a7d14279d3e60f8c2ac5f37ea769f91df
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-05 16:20:33 +0700

    fix(web): entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,4 @@
-RUN npm run build
+npm run build
 if [ `id -u` -eq 0 ] && [ -n "$PUID" ] && [ -n "$PGID" ]; then
     exec setpriv --reuid $PUID --regid $PGID --clear-groups node /usr/src/app/build/index.js
 else
